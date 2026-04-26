c192408f858e215de537aa612e59696b635f681f
nlp4everyone <luonghongphong270498@gmail.com>
2026-04-26 16:25:07 +0700

    refactor: Migrate from ASR to VAD service
    - Rename DEPLOYMENT_NAME to VAD_DEPLOYMENT_NAME
    - Replace ASR model config with VAD model (pyannote/segmentation-3.0)
    - Update serving parameters for VAD workload
    - Remove ASR-specific batch processing configs

--- app/core/config/system.py
+++ app/core/config/system.py
@@ -7,6 +7,6 @@ system_config = toml_config.get_section("system")
 
 # Model config
 AUDIO_TEMP_DIR = system_config.get("AUDIO_TEMP_DIR", "/dev/shm")
-DEPLOYMENT_NAME = system_config.get("DEPLOYMENT_NAME", "VADService")
+VAD_DEPLOYMENT_NAME = system_config.get("VAD_DEPLOYMENT_NAME", "VADService")
 RAY_HOST = system_config.get("RAY_HOST", "0.0.0.0")
 RAY_PORT = system_config.get("RAY_PORT", 8000)
